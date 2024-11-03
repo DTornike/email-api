3cbd7801ade71a3865b40bab0abd2943a2918778
Tornike <tornike.mamatsashvili.1@gmail.com>
2024-11-03 19:44:29 +0400

    Add get current user method

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import UserListView
+from .views import CurrentUserView
 
 urlpatterns = [
     path('list/', UserListView.as_view(), name='user-list'),
+    path('current-user/', CurrentUserView.as_view(), name='current-user'),
 ]
--- users/views.py
+++ users/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 
 class RegisterView(generics.CreateAPIView):
@@ -15,3 +17,12 @@ class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+
+class CurrentUserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
